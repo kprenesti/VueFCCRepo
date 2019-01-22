2866aa3b7d4ccb66600b59d9110c394931a367b4
Krista Prenesti <kprenesti@gmail.com>
2019-01-21 22:08:28 -0500

    Password validation pt 1

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const gravatar = require('gravatar');
-
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -16,11 +16,15 @@ router.get('/test', (req, res) => res.json({
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
-router.post('/register', (req, res)=> {
-  User.findOne({email: req.body.email})
-    .then((user)=>{
-      if(user){ 
-        res.status(400).json({msg: "Email already in use.  Please login."})
+router.post('/register', (req, res) => {
+  User.findOne({
+      email: req.body.email
+    })
+    .then((user) => {
+      if (user) {
+        res.status(400).json({
+          msg: "Email already in use.  Please login."
+        })
       } else {
         const avatar = gravatar.url(req.body.email, {
           s: '200',
@@ -31,10 +35,51 @@ router.post('/register', (req, res)=> {
           name: req.body.name,
           email: req.body.email,
           password: req.body.password,
-          // avatar
+          avatar
         });
+
+        // Encrypt password and save user to Mongo
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newbie.password, salt, (err, hash) => {
+            if (err) throw err;
+            newbie.password = hash;
+            newbie.save()
+              .then(user => res.json(user))
+              .catch(err => console.error(err));
+          })
+        })
       }
     })
-  });
+});
+
+
+// @route   POST api/users/login
+// @desc    Login User / Return JSON Web Token
+// @access  Public
+router.post('/login', (req, res) => {
+  // Find user by email address.
+  User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      if (!user) return res.status(400).json({
+        msg: 'User not found.'
+      });
+
+      // Compare the given pw with the stored, hashed version.  
+      bcrypt.compare(req.body.password, user.password)
+        .then(match => {
+          if (match) {
+            res.json({
+              msg: "User found! Woo!"
+            });
+          } else {
+            return res.status(400).json({
+              msg: 'Password Incorrect.'
+            })
+          }
+        })
+    })
+})
 
 module.exports = router;
