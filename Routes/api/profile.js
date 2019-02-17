eea6eceb4281181aeabed7217356b569ee099ac0
Krista Prenesti <krista.prenesti@gmail.com>
2019-02-17 15:51:33 -0500

    Added the Get Profile route

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -1,6 +1,10 @@
 // Keeps user personal info
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
 // @route   GET api/profile/test
@@ -9,5 +13,24 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: "Profile works."
 }));
+
+// @route   GET api/profile
+// @desc    Gets current user's profile.  Get's user's id from token
+// @access  Private
+router.get('/', passport.authenticate('jwt'), (req, res) => {
+  const errors = {};
+  Profile.findOne({
+      user: req.user.id
+    })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "No profile found.";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    }).catch((err) => {
+      res.json(err);
+    })
+});
 
 module.exports = router;
