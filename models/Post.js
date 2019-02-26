87a4c8def6c4d31c33a864c285e793a029427b7a
Krista Prenesti <kprenesti@gmail.com>
2019-02-25 21:12:31 -0500

    Added Post Schema

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    }
+  }],
+  comments: [{
+    user: {
+      type: Schema.Types.ObjectId
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String
+    },
+    avatar: {
+      type: String
+    },
+    date: {
+      type: Date,
+      defualt: Date.now
+    }
+  }]
+})
+
+modules.exports = mongoose.model('post', PostSchema);
