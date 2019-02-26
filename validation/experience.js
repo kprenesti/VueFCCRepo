979f565810d920ad292f48ea392e4c76c7f79f95
Krista Prenesti <kprenesti@gmail.com>
2019-02-25 20:53:07 -0500

    Added education and experience validation files

---
+++ validation/experience.js
@@ -0,0 +1,31 @@
+const validator = require('validator');
+const isEmpty = require('./customFunctions').isEmpty;
+
+const validateExperienceInput = (data) => {
+  let errors = {};
+  data.title = !isEmpty(data.title) ? data.title : '';
+  data.company = !isEmpty(data.company) ? data.company : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Job title field is required';
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = 'Company field is required';
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From date field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+
+
+};
+
+
+modules.export = validateExperienceInput;
