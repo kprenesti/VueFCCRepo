7f0ee97f4c06d1d8ea54a6bf96c098304ed94ac2
Krista Prenesti <kprenesti@gmail.com>
2019-03-01 12:09:56 -0500

    Fixed concurrently error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9090;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
