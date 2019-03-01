61fdd3ea964769efaa270b0aa28daac700536cf7
Krista Prenesti <kprenesti@gmail.com>
2019-03-01 12:39:32 -0500

    Fixed formatting conflicts with Prettier and Beautify by removing Beautify

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      <
-      h1 > This is a test of the emergency React system. < /h1> <
-      /div>
+    return (
+      <div className="App">
+        <h1> This is a test of the emergency React system.</h1>
+      </div>
     );
   }
 }
